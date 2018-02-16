61543a375a78074e8c19e21fc7e243fb056af6a5
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-16 23:37:34 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -16,11 +16,11 @@ def apply_coupons(cart, coupons)
   couponfoods = []
   cart.each do |food, info|
     coupons.each do |coupon|
-      if food == coupon[:item] 
-        if couponfoods.include?(food) && newcart[food][:count] > coupon[:num]
+      if food == coupon[:item] #if i have a coupon for one of my foods
+        if couponfoods.include?(food) && newcart[food][:count] > coupon[:num] #condition for multiple of the same coupon
           newcart["#{food} W/COUPON"][:count] += 1 
           newcart[food][:count] = newcart[food][:count] - coupon[:num]
-        elsif newcart[food][:count] > coupon[:num]
+        elsif newcart[food][:count] > coupon[:num] #otherwise just apply coupon, add coupon food to my list of coupon foods
           couponfoods << food
           newcart["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[food][:clearance], :count => coupon[:num]/coupon[:num]}
           newcart[food][:count] = newcart[food][:count] - coupon[:num]
@@ -30,6 +30,7 @@ def apply_coupons(cart, coupons)
   end
   newcart
 end
+
 
 def apply_clearance(cart)
   # code here
